e84ed5c3cd0e179de1081b0025a3c51c6493c371
Sarah Olivatto <osarah381@gmail.com>
2026-03-10 15:29:40 -0300

    ta funcionando usuario - outros botoes nao

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+let usuarios = [
+    { id: 1, nome: "João", email: "joao@email.com" },
+    { id: 2, nome: "Maria", email: "maria@email.com" },
+];
 
 //Rota principal
 app.get('/', (req, res) => {
@@ -41,18 +45,43 @@ app.get('/usuario', (req, res) => {
 
 // MOSTRAR
 app.get('/usuario/show/:id', (req, res) => {
-    const userId = req.params.id;
-    res.render('usuario/show', { userId });
+
+    const user = {
+        id: req.params.id,
+        nome: "João da Silva",
+        email: "joao@email.com",
+        data_nascimento: new Date(1990, 5, 10)
+    };
+
+    res.render('usuario/show', { user });
+
 });
 
 // EDITAR
 app.get('/usuario/edit/:id', (req, res) => {
-    const userId = req.params.id;
-    res.render('usuario/edit', { userId });
+    
+    const user = {
+        id: req.params.id,
+        nome: "João da Silva",
+        email: "joao@email.com",
+        data_nascimento: new Date(1990, 5, 10)
+    };
+
+    res.render('usuario/edit', { user });
 });
 
+app.post('/usuario/edit/:id', (req, res) => {
+    
+    const user = usuarios.find(u => u.id == req.params.id);
+    if (user) {
+        user.nome = req.body.nome;
+        user.email = req.body.email;
+    }
+
+    res.redirect('/usuario')
+});
 // ADICIONAR
-app.get('/Usuario/add', (req, res) => {
+app.get('/usuario/add', (req, res) => {
     res.render('usuario/add');
 });
 
--- routes/Usuario.js
+++ routes/Usuario.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/show/:id", (req, res) => {
+router.get("/usuario/show/:id", (req, res) => {
     const userId = req.params.id;
 
     const user = getUserById(userId); 
 
     res.render("usuario/show", { user: user });
 });
-
 module.exports = router;
