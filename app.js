f405cf06dffa4f99d91c24e879e0c5246a74a782
Sarah Olivatto <osarah381@gmail.com>
2026-03-17 13:56:03 -0300

    meucarro e usuario ok

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Servir arquivos estáticos da pasta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
-
 //configuração do EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-let usuarios = [
+let usuario = [
     { id: 1, nome: "João", email: "joao@email.com" },
     { id: 2, nome: "Maria", email: "maria@email.com" },
+];
+
+let meucarro = [
+    {id: 1, modelo: "Mazda-MX7", ano: "1997", cod_obd2: "elm20183eth"},
+    {id: 2, modelo: "Jetta GLI", ano: "2018", cod_obd2: "elm3916vat"},
 ];
 
 //Rota principal
@@ -28,8 +30,23 @@ app.get('/', (req, res) => {
 //Importando as rotas
 
 //Rota para a pagina MeuCarro
-app.get('/views/MeuCarro', (req, res) => {
-    res.render('MeuCarro');
+app.get('/meucarro', (req, res) => {
+    const user = meucarro[0]; 
+    res.render('meucarro/index', { user });
+});
+
+app.post('/meucarro/add', (req, res) => {
+
+    const novoCarro = {
+        id: meucarro.length + 1,
+        modelo: req.body.modelo,
+        ano: req.body.ano,
+        cod_obd2: req.body.cod_obd2
+    };
+
+    meucarro.push(novoCarro);
+
+    res.redirect('/meucarro');
 });
 
 //Rota para a pagina Monitoramento
@@ -40,18 +57,20 @@ app.get('/views/Monitoramento', (req, res) => {
 //Rota para a pagina Usuário
 // LISTAR
 app.get('/usuario', (req, res) => {
-    res.render('usuario/index');
+    const user = usuario[0]; 
+    res.render('usuario/index', { user });
 });
 
 // MOSTRAR
 app.get('/usuario/show/:id', (req, res) => {
 
     const user = {
-        id: req.params.id,
-        nome: "João da Silva",
-        email: "joao@email.com",
-        data_nascimento: new Date(1990, 5, 10)
+         id: usuario.length + 1,
+        modelo: req.body.nome,
+        ano: req.body.email
     };
+
+    meucarro.push(user);
 
     res.render('usuario/show', { user });
 
@@ -90,6 +109,10 @@ app.get('/usuario/add', (req, res) => {
 app.get('/views/Histórico', (req, res) => {
     res.render('Histórico');
 });
+
+
+// ROTA MEUCARRO
+
 
 //Iniciando o servidor
 app.listen(port, () => {
--- routes/MeuCarro.js
+++ routes/MeuCarro.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/meucarro/show/:id", (req, res) => {
+    const userId = req.params.id;
+
+    const user = getUserById(userId); 
+
+    res.render("meucarro/show", { user: user });
+});
+module.exports = router;
