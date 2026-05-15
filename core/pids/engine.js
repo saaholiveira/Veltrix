e655f623e74d7aa8c30ccff6e46e78e12d9633af
Sarah Olivatto <osarah381@gmail.com>
2026-05-15 16:28:49 -0300

    teste

--- core/pids/engine.js
+++ core/pids/engine.js
@@ -1,5 +1,7 @@
 function calcularRPM(resposta) {
+
     const bytes = resposta.split(' ');
+
     const A = parseInt(bytes[2], 16);
     const B = parseInt(bytes[3], 16);
 
@@ -7,13 +9,44 @@ function calcularRPM(resposta) {
 }
 
 function calcularTemperatura(resposta) {
+
     const bytes = resposta.split(' ');
+
     const A = parseInt(bytes[2], 16);
 
     return A - 40;
 }
 
+function calcularCombustivel(resposta) {
+
+    const bytes = resposta.split(' ');
+
+    const A = parseInt(bytes[2], 16);
+
+    return (A * 100) / 255;
+}
+
+function calcularVelocidade(resposta) {
+
+    const bytes = resposta.split(' ');
+
+    return parseInt(bytes[2], 16);
+}
+
+function calcularBateria(resposta) {
+
+    const bytes = resposta.split(' ');
+
+    const A = parseInt(bytes[2], 16);
+    const B = parseInt(bytes[3], 16);
+
+    return ((A * 256) + B) / 1000;
+}
+
 module.exports = {
     calcularRPM,
-    calcularTemperatura
+    calcularTemperatura,
+    calcularCombustivel,
+    calcularVelocidade,
+    calcularBateria
 };
